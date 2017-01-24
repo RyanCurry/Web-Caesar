b6c581cf71a9973541383a7d08ff58316d03610f
Ryan Curry <rtc1995@yahoo.com>
2017-01-24 13:22:34 -0600

    added text area and number input

--- main.py
+++ main.py
@@ -16,16 +16,30 @@
 #
 import webapp2
 import Caesar
+import cgi
+
+def build_page(content):
+    encryptbox="<textarea name='message'>" + content + "</textarea>"
+    button="<input Type='submit'/>"
+    rotate="<input Type='number' name='rot'/>"
+    message_label="<label>Type a message:</label>"
+    rot_label="<label>Pick a number to rotate by:</label>"
+    form="<form method='post'>" + rot_label + rotate + '<br>'  + message_label + '<br>'  + encryptbox + "<br>" + button + "</form>"
+    header="<h2>Web Caesar</h2>"
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.write(build_page(""))
 
-        mes="Hello world!"
-        encryptedmes=Caesar.encrypt(mes,13)
-        encryptbox="<textarea>"+ encryptedmes + "</textarea>"
-        button="<input Type='submit'/>"
-        form="<form>" + encryptbox + "<br>" + button + "</form>"
-        self.response.write(form)
+    def post(self):
+        mes= self.request.get("message")
+        rotation= self.request.get("rot")
+        encryptedmes=Caesar.encrypt(mes,rotation)
+
+        escaped_message=cgi.escape(encryptedmes)
+
+        self.response.write(build_page(escaped_message))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
